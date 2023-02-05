a36acd209e1926631410e525ebcd0656b17b6a32
Project Nayuki <me@nayuki.io>
2023-02-05 22:29:04 +0000

    Added newline between input bits and output bytes when calling test().

--- test/io/nayuki/deflate/InflaterInputStreamTest.java
+++ test/io/nayuki/deflate/InflaterInputStreamTest.java
@@ -63,35 +63,40 @@ public final class InflaterInputStreamTest {
 	@Test(expected=EOFException.class)
 	public void testEofStartOfBlock() throws IOException {
 		// No blocks
-		test("", "");
+		test("",
+			"");
 	}
 	
 	
 	@Test(expected=DataFormatException.class)
 	public void testReservedBlockType() throws IOException {
 		// Reserved block type
-		test("1 11 00000", "");
+		test("1 11 00000",
+			"");
 	}
 	
 	
 	@Test(expected=EOFException.class)
 	public void testEofInBlockType() throws IOException {
 		// Partial block type
-		test("1 0", "");
+		test("1 0",
+			"");
 	}
 	
 	
 	@Test
 	public void testUncompressedEmpty() throws IOException {
 		// Uncompressed block len=0: (empty)
-		test("1 00 00000   0000000000000000 1111111111111111", "");
+		test("1 00 00000   0000000000000000 1111111111111111",
+			"");
 	}
 	
 	
 	@Test
 	public void testUncompressedThreeBytes() throws IOException {
 		// Uncompressed block len=3: 05 14 23
-		test("1 00 00000   1100000000000000 0011111111111111   10100000 00101000 11000100", "05 14 23");
+		test("1 00 00000   1100000000000000 0011111111111111   10100000 00101000 11000100",
+			"05 14 23");
 	}
 	
 	
@@ -99,35 +104,40 @@ public final class InflaterInputStreamTest {
 	public void testUncompressedTwoBlocks() throws IOException {
 		// Uncompressed block len=1: 05
 		// Uncompressed block len=2: 14 23
-		test("0 00 00000   0100000000000000 1011111111111111   10100000 00101000   1 00 00000   1000000000000000 0111111111111111   11000100", "05 14 23");
+		test("0 00 00000   0100000000000000 1011111111111111   10100000 00101000   1 00 00000   1000000000000000 0111111111111111   11000100",
+			"05 14 23");
 	}
 	
 	
 	@Test(expected=EOFException.class)
 	public void testUncompressedEofBeforeLength() throws IOException {
 		// Uncompressed block (partial padding) (no length)
-		test("1 00 000", "");
+		test("1 00 000",
+			"");
 	}
 	
 	
 	@Test(expected=EOFException.class)
 	public void testUncompressedEofInLength() throws IOException {
 		// Uncompressed block (partial length)
-		test("1 00 00000 0000000000", "");
+		test("1 00 00000 0000000000",
+			"");
 	}
 	
 	
 	@Test(expected=DataFormatException.class)
 	public void testUncompressedMismatchedLength() throws IOException {
 		// Uncompressed block (mismatched len and nlen)
-		test("1 00 00000 0010000000010000 1111100100110101", "");
+		test("1 00 00000 0010000000010000 1111100100110101",
+			"");
 	}
 	
 	
 	@Test(expected=EOFException.class)
 	public void testUncompressedEofInData() throws IOException {
 		// Uncompressed block len=6: 55 EE (End)
-		test("1 00 11111 0110000000000000 1001111111111111 10101010 01110111", "");
+		test("1 00 11111 0110000000000000 1001111111111111 10101010 01110111",
+			"");
 	}
 	
 	
@@ -135,7 +145,8 @@ public final class InflaterInputStreamTest {
 	public void testUncompressedBlockNoFinalBlock() throws IOException {
 		// Uncompressed block len=0: (empty)
 		// No final block
-		test("0 00 00000   0000000000000000 1111111111111111", "");
+		test("0 00 00000   0000000000000000 1111111111111111",
+			"");
 	}
 	
 	
@@ -143,91 +154,104 @@ public final class InflaterInputStreamTest {
 	public void testUncompressedBlockNoDiscardBits() throws IOException {
 		// Fixed Huffman block: 90 A1 FF End
 		// Uncompressed block len=2: AB CD
-		test("0 10 110010000 110100001 111111111 0000000  1 00 0100000000000000 1011111111111111 11010101 10110011", "90 A1 FF AB CD");
+		test("0 10 110010000 110100001 111111111 0000000  1 00 0100000000000000 1011111111111111 11010101 10110011",
+			"90 A1 FF AB CD");
 	}
 	
 	
 	@Test
 	public void testFixedHuffmanEmpty() throws IOException {
 		// Fixed Huffman block: End
-		test("1 10 0000000", "");
+		test("1 10 0000000",
+			"");
 	}
 	
 	
 	@Test
 	public void testFixedHuffmanLiterals() throws IOException {
 		// Fixed Huffman block: 00 80 8F 90 C0 FF End
-		test("1 10 00110000 10110000 10111111 110010000 111000000 111111111 0000000", "00 80 8F 90 C0 FF");
+		test("1 10 00110000 10110000 10111111 110010000 111000000 111111111 0000000",
+			"00 80 8F 90 C0 FF");
 	}
 	
 	
 	@Test
 	public void testFixedHuffmanNonOverlappingRun() throws IOException {
 		// Fixed Huffman block: 00 01 02 (3,3) End
-		test("1 10 00110000 00110001 00110010 0000001 00010 0000000", "00 01 02 00 01 02");
+		test("1 10 00110000 00110001 00110010 0000001 00010 0000000",
+			"00 01 02 00 01 02");
 	}
 	
 	
 	@Test
 	public void testFixedHuffmanOverlappingRun0() throws IOException {
 		// Fixed Huffman block: 01 (1,4) End
-		test("1 10 00110001 0000010 00000 0000000", "01 01 01 01 01");
+		test("1 10 00110001 0000010 00000 0000000",
+			"01 01 01 01 01");
 	}
 	
 	
 	@Test
 	public void testFixedHuffmanOverlappingRun1() throws IOException {
 		// Fixed Huffman block: 8E 8F (2,5) End
-		test("1 10 10111110 10111111 0000011 00001 0000000", "8E 8F 8E 8F 8E 8F 8E");
+		test("1 10 10111110 10111111 0000011 00001 0000000",
+			"8E 8F 8E 8F 8E 8F 8E");
 	}
 	
 	
 	@Test(expected=DataFormatException.class)
 	public void testFixedHuffmanInvalidLengthCode286() throws IOException {
 		// Fixed Huffman block: #286
-		test("1 10 11000110", "");
+		test("1 10 11000110",
+			"");
 	}
 	
 	
 	@Test(expected=DataFormatException.class)
 	public void testFixedHuffmanInvalidLengthCode287() throws IOException {
 		// Fixed Huffman block: #287
-		test("1 10 11000111", "");
+		test("1 10 11000111",
+			"");
 	}
 	
 	
 	@Test(expected=DataFormatException.class)
 	public void testFixedHuffmanInvalidDistanceCode30() throws IOException {
 		// Fixed Huffman block: 00 #257 #30
-		test("1 10 00110000 0000001 11110", "");
+		test("1 10 00110000 0000001 11110",
+			"");
 	}
 	
 	
 	@Test(expected=DataFormatException.class)
 	public void testFixedHuffmanInvalidDistanceCode31() throws IOException {
 		// Fixed Huffman block: 00 #257 #31
-		test("1 10 00110000 0000001 11111", "");
+		test("1 10 00110000 0000001 11111",
+			"");
 	}
 	
 	
 	@Test(expected=EOFException.class)
 	public void testFixedHuffmanEofInHuffmanSymbol() throws IOException {
 		// Fixed Huffman block: (partial symbol)
-		test("1 10 00000", "");
+		test("1 10 00000",
+			"");
 	}
 	
 	
 	@Test(expected=EOFException.class)
 	public void testFixedHuffmanEofInRunExtensionBits() throws IOException {
 		// Fixed Huffman block: 00 #269+1(partial)
-		test("1 10 00110000 0001101 1", "");
+		test("1 10 00110000 0001101 1",
+			"");
 	}
 	
 	
 	@Test(expected=EOFException.class)
 	public void testFixedHuffmanEofInDistanceExtensionBits() throws IOException {
 		// Fixed Huffman block: 00 #285 #0 #257 #8+00(partial)
-		test("1 10 00110000 11000101 00000 0000001 01000 00", "");
+		test("1 10 00110000 11000101 00000 0000001 01000 00",
+			"");
 	}
 	
 	
@@ -245,7 +269,8 @@ public final class InflaterInputStreamTest {
 		String codeLenCodeLens = "000 000 100 000 000 000 000 000 000 000 000 000 000 000 000 000 000 100 000";
 		String codeLens = "0 11111111 10101011 0 0 0";
 		String data = "1";
-		test(blockHeader + codeCounts + codeLenCodeLens + codeLens + data, "");
+		test(blockHeader + codeCounts + codeLenCodeLens + codeLens + data,
+			"");
 	}
 	
 	
@@ -263,7 +288,8 @@ public final class InflaterInputStreamTest {
 		String codeLenCodeLens = "000 000 100 010 000 000 000 000 000 000 000 000 000 000 000 000 000 010";
 		String codeLens = "01111111 00101011 11 11 10";
 		String data = "1";
-		test(blockHeader + codeCounts + codeLenCodeLens + codeLens + data, "");
+		test(blockHeader + codeCounts + codeLenCodeLens + codeLens + data,
+			"");
 	}
 	
 	
@@ -277,7 +303,8 @@ public final class InflaterInputStreamTest {
 		String codeCounts = "00000 00000 0111";
 		String codeLenCodeLens = "100 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 000 100";
 		String codeLens = "1";
-		test(blockHeader + codeCounts + codeLenCodeLens + codeLens, "");
+		test(blockHeader + codeCounts + codeLenCodeLens + codeLens,
+			"");
 	}
 	
 	
@@ -291,7 +318,8 @@ public final class InflaterInputStreamTest {
 		String codeCounts = "00000 00000 0111";
 		String codeLenCodeLens = "000 000 100 000 000 000 000 000 000 000 000 000 000 000 000 000 000 100";
 		String codeLens = "0 0 11111111 10011011";
-		test(blockHeader + codeCounts + codeLenCodeLens + codeLens, "");
+		test(blockHeader + codeCounts + codeLenCodeLens + codeLens,
+			"");
 	}
 	
 	
@@ -304,7 +332,8 @@ public final class InflaterInputStreamTest {
 		String codeCounts = "00000 00000 0000";
 		String codeLenCodeLens = "100 100 100 000";
 		String padding = "0000000000000000000";
-		test(blockHeader + codeCounts + codeLenCodeLens + padding, "");
+		test(blockHeader + codeCounts + codeLenCodeLens + padding,
+			"");
 	}
 	
 	
@@ -317,7 +346,8 @@ public final class InflaterInputStreamTest {
 		String codeCounts = "00000 00000 0000";
 		String codeLenCodeLens = "100 100 100 100";
 		String padding = "0000000000000000000";
-		test(blockHeader + codeCounts + codeLenCodeLens + padding, "");
+		test(blockHeader + codeCounts + codeLenCodeLens + padding,
+			"");
 	}
 	
 	
@@ -330,7 +360,8 @@ public final class InflaterInputStreamTest {
 		String codeCounts = "00000 00000 0000";
 		String codeLenCodeLens = "100 010 110 000";
 		String padding = "0000000000000000000";
-		test(blockHeader + codeCounts + codeLenCodeLens + padding, "");
+		test(blockHeader + codeCounts + codeLenCodeLens + padding,
+			"");
 	}
 	
 	
@@ -343,7 +374,8 @@ public final class InflaterInputStreamTest {
 		String codeCounts = "00000 00000 0000";
 		String codeLenCodeLens = "000 000 000 000";
 		String padding = "0000000000000000000";
-		test(blockHeader + codeCounts + codeLenCodeLens + padding, "");
+		test(blockHeader + codeCounts + codeLenCodeLens + padding,
+			"");
 	}
 	
 	
@@ -356,7 +388,8 @@ public final class InflaterInputStreamTest {
 		String codeCounts = "00000 00000 0000";
 		String codeLenCodeLens = "000 000 100 000";
 		String padding = "0000000000000000000";
-		test(blockHeader + codeCounts + codeLenCodeLens + padding, "");
+		test(blockHeader + codeCounts + codeLenCodeLens + padding,
+			"");
 	}
 	
 	
@@ -369,7 +402,8 @@ public final class InflaterInputStreamTest {
 		String codeCounts = "00000 00000 0000";
 		String codeLenCodeLens = "010 100 000 000";
 		String padding = "0000000000000000000";
-		test(blockHeader + codeCounts + codeLenCodeLens + padding, "");
+		test(blockHeader + codeCounts + codeLenCodeLens + padding,
+			"");
 	}
 	
 	
@@ -386,7 +420,8 @@ public final class InflaterInputStreamTest {
 		String codeLens = "10 111111111 110101011 01 10 00";
 		String data = "10 11";
 		String padding = "0000000000000000";
-		test(blockHeader + codeCounts + codeLenCodeLens + codeLens + data + padding, "");
+		test(blockHeader + codeCounts + codeLenCodeLens + codeLens + data + padding,
+			"");
 	}
 	
 	
